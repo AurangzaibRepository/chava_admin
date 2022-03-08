23376e2e31ae2759397fa40bdf27584fc151a87f
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-08 13:40:45 +0500

    Adds cache meta tags in main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <meta http-equiv='cache-control' content='no-cache'/>
+    <meta http-equiv='cache-control' content='no-cache' />
     <meta http-equiv='expires' content='0' />
     <meta http-equiv='pragma' content='no-cache' />
     <meta name="csrf-token" content="{{csrf_token()}}" />
