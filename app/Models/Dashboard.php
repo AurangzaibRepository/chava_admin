e8c1019dfcfa2a94c1e5d9afea645d870edf5561
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-08 12:11:02 +0500

    Implements function to get activity chart data

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -2,13 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\MOdel;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class Dashboard extends Model
 {
     public function getActivityData(): JsonResponse
     {
-        echo 'in here'; exit;
+        $response = [];
+
+        $activeUserCount = User::where('status', 'Active')
+                                ->where('role', '!=', 'Admin')
+                                ->count();
+
+        $inactiveUserCount = User::where('status', 'Inactive')
+                                ->count();
+
+        /*$userStatusCount = DB::table('users')
+                             ->selectRaw('status, count(id) count')
+                             ->where('status', '!=', 'Admin')
+                             ->groupBy('status')
+                             ->orderBy('status')
+                             ->get();*/
+
+        $response['active_users'] = $activeUserCount;
+        $response['inactive_users'] = $inactiveUserCount;
+        $response['new_users'] = 0;
+
+        return response()->json($response);
     }
 }
