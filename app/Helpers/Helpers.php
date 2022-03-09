7d7a54b82d863545628f3ed0ae889231290aff73
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-09 13:48:08 +0500

    Adds Carbon namespace in helper

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+
+function calculateLastActivity(string $lastActiveDate): string
+{
+    $currentDate = Carbon::now();
+
+    $dateDiff = Carbon::parse($lastActiveDate)->diff($currentDate);
+    $dateDiff = $dateDiff->format('%y-%m-%d-%h-%m');
+    $dateDiff = Str::of($dateDiff)->split('/[\s-]+/');
+
+    if ($dateDiff[0] > 0){
+        //$lastActive = Str::of($lastActive)->append("{$dateDiff[0]} years ");
+        return "{$dateDiff[0]} years ago";
+    }
+
+    if ($dateDiff[1] > 0){
+        return "{$dateDiff[1]} months ago";
+    }
+
+    if ($dateDiff[2] > 0){
+        return "{$dateDiff[2]} days ago";
+    }
+
+    if ($dateDiff[3] > 0){
+        return "{$dateDiff[3]} hours ago";
+    }
+
+    if ($dateDiff[4] > 0){
+        return "{$dateDiff[4]} minutes ago";
+    }
+}
