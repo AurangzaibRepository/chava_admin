e21902173dbeebd84e1757b52f698c64df21188e
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-09 13:10:45 +0500

    Implements function to calculate last active duration

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
@@ -43,9 +45,43 @@ class Dashboard extends Model
                     ->get();
 
         foreach ($userList as $user) {
-            $data[] = [$user->user_name, $user->last_active];
+           $lastActive = $this->calculateLastActivity($user->last_active);
+
+            $data[] = [$user->user_name, $lastActive];
         }
         
         return new Collection($data);
+    }
+
+
+    private function calculateLastActivity(string $lastActiveDate): string
+    {
+        $currentDate = Carbon::now();
+
+        $dateDiff = Carbon::parse($lastActiveDate)->diff($currentDate);
+        $dateDiff = $dateDiff->format('%y-%m-%d-%h-%m');
+        $dateDiff = Str::of($dateDiff)->split('/[\s-]+/');
+
+        if ($dateDiff[0] > 0){
+            //$lastActive = Str::of($lastActive)->append("{$dateDiff[0]} years ");
+            return "{$dateDiff[0]} years ago";
+        }
+
+        if ($dateDiff[1] > 0){
+            return "{$dateDiff[1]} months ago";
+        }
+
+        if ($dateDiff[2] > 0){
+            return "{$dateDiff[2]} days ago";
+        }
+
+        if ($dateDiff[3] > 0){
+            return "{$dateDiff[3]} hours ago";
+        }
+
+        if ($dateDiff[4] > 0){
+            return "{$dateDiff[4]} minutes ago";
+        }
+
     }
 }
