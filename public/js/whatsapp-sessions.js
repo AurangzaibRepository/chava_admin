ee6e11c73cf6f12744b6979b07a802ca5b152ed3
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-14 17:06:26 +0500

    Creates script file for whats app session

---
+++ public/js/whatsapp-sessions.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    PopulateUsers();
+});
+
+function PopulateUsers()
+{
+    $('#table-whatsapp').DataTable({
+        'bLengthChange': false,
+        'bSort': false,
+        'searching': false,
+        'language': {
+            'emptyTable': 'No records found'
+        },
+        columnDefs: [
+            {targets:[0,1,2,3], width: '25%'},
+        ]
+    });
+}
