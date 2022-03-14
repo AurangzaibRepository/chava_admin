94b5fd16ead74c833f867cef87fa55ab2ef50aac
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-14 19:06:23 +0500

    Creates separate function to get users count

--- app/Models/WhatsappSession.php
+++ app/Models/WhatsappSession.php
@@ -2,8 +2,39 @@
 
 namespace App\Models;
 
-use Illumitnae\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class WhatsappSession extends Model
 {
+    public function getListing(Request $request): JsonResponse
+    {
+        $data = [
+            'draw' => $request->draw,
+            'data' => []
+        ];
+
+        $data['recordsTotal'] = $this->getUsersCount();
+
+        $data['recordsFiltered'] = $data['recordsTotal'];
+
+        $userList = DB::table('users')
+                        ->where('role', '!=', 'Admin')
+                        ->limit(10)
+                        ->offset($request->start)
+                        ->orderBy('id', 'desc');
+
+
+
+        return response()->json($data);
+    }
+
+    private function getUsersCount(): int
+    {
+        return DB::table('users')
+                ->where('role', '!=', 'Admin')
+                ->count();
+    }
 }
