757253cffb9eaf84edd11af09f1fe10532df2c3f
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-14 20:06:01 +0500

    Add whatsapp icon with link to user number

--- public/js/whatsapp-sessions.js
+++ public/js/whatsapp-sessions.js
@@ -15,7 +15,11 @@ function PopulateUsers()
         },
         columnDefs: [
             {targets:[0,1,2], width: '30%'},
-            {targets: 3, width: '10%'}
+            {targets: 3, width: '10%', data:null,
+            render: function(data, type, row, meta){
+                return `<a href='https://web.whatsapp.com/send?phone=${data[2]}' target='_blank'> <i class="fab fa-whatsapp"></i></a>`
+            }
+        }
         ],
         ajax: '/whatsapp-sessions/listing'
     });
