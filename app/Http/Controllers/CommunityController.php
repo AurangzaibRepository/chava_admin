6ac1957e04a35abb491aaa9975ec97e47f722028
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-18 10:59:49 +0500

    Includes model via property promotion

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminte\Http\Request;
 use Illuminate\View\View;
+use App\Models\CommunityFeed;
 use Carbon\Carbon;
 
 class CommunityController extends Controller
 {
+    public function __construct(
+        private CommunityFeed $communityFeed
+    ) {
+    }
+
     // Default function
     public function index(): view
     {
         $pageTitle = 'Community';
         $date = Carbon::now()->format('d F, l');
+
+        
 
         return view('community', [
             'pageTitle' => $pageTitle,
