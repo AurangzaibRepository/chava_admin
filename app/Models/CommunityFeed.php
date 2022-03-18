f718226341bd8a6cfc92d7be69f00182cecad11f
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-18 12:48:03 +0500

    Formats date

--- app/Models/CommunityFeed.php
+++ app/Models/CommunityFeed.php
@@ -8,13 +8,15 @@ use Illuminate\Support\Collection;
 
 class CommunityFeed extends Model
 {
+    protected $table = 'community_feeds';
+
     public function getLatest(): Collection
     {
         $feedList = DB::table('community_feeds')
                         ->join('users', 'users.id', 'community_feeds.user_id')
                         ->orderBy('community_feeds.id', 'desc')
                         ->limit(3)
-                        ->select('community_feeds.*', 'users.user_name')
+                        ->selectRaw('community_feeds.*, date_format(community_feeds.createdAt, "%M %d, %Y") createdAt, users.user_name')
                         ->get();
 
         return $feedList;
