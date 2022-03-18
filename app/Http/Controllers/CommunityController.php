0cece952bad6b0d19686809b7d57b5f5a5114360
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-19 01:47:50 +0500

    Implements function to retrieve categories

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -5,21 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use App\Models\CommunityFeed;
+use App\Models\Category;
 use Carbon\Carbon;
 
 class CommunityController extends Controller
 {
     public function __construct(
-        private CommunityFeed $communityFeed
+        private CommunityFeed $communityFeed,
+        private Category $category
     ) {
     }
 
     // Default function
     public function index(): view
     {
+        $categoryList = $this->category->getListing()->pluck('category', 'id');
+
         return view('community', [
             'pageTitle' => 'Community',
             'date' => Carbon::now()->format('d F, l'),
+            'categories' => $categoryList->prepend('-- Select --', ''),
             'data' => $this->communityFeed->getLatest()
         ]);
     }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Category extends Model
 {
     protected $table = 'categories';
+
+    public function getListing(): Collection
+    {
+        $data = $this
+                    ->orderBy('id')
+                    ->get();
+
+        return $data;
+    }
 }
