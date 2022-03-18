d9b3b9af6b51f71dd973c9beea38adc8088d5762
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-18 14:47:55 +0500

    Implements function to changestatus in script file

--- public/js/community.js
+++ public/js/community.js
@@ -2,3 +2,15 @@ $(document).ready(function(){
 
     
 })
+
+function changeStatus(feedID, status, answer = null)
+{
+    $.post('/community/change-status', {
+        feedID: feedID,
+        status: status,
+        answer: answer
+    }, 
+    function(response){
+        window.location = '/community';
+    });
+}
