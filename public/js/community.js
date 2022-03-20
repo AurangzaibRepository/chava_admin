066dbdacb4abfb588c3610423e9c821cc868ab9b
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-20 22:53:51 +0500

    Gets feedid on modal open

--- public/js/community.js
+++ public/js/community.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     $('select').select2({
         dropdownParent: $('#modal-approval')
     });
+
+    $('#modal-approval').on('show.bs.modal', function(e){
+        var feedID = $(e.relatedTarget).data('id');
+
+        alert(feedID);
+    });
 });
 
 function changeStatus(feedID, status, answer = null)
