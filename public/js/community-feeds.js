e6b6e240c0ed0dc5f43ec6da5db07ff7fe476fac
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-21 15:14:39 +0500

    Adds config to render columns by name

--- public/js/community-feeds.js
+++ public/js/community-feeds.js
@@ -1,17 +1,32 @@
+var table;
+var i = 1;
+
 $(function(){
 
    populateFeeds();
 });
 
+// Bind datatable page click
+$(document).bind('click', 'div.pagination li', function(evt){
+    i = ((table.page.info().page + 1) * 10) - 9;
+});
+
 function populateFeeds()
 {
-    $('#table-feeds').DataTable({
+    table = $('#table-feeds').DataTable({
         'searching': false,
         'bLengthChange': false,
         'bSort': false,
         'serverSide': true,
         'columnDefs': [
-            {'targets': 0, 'width':'8%', 'class': 'text-center'},
+            {'targets': 0, 'width':'8%', 'class': 'text-center',
+                'render': function() {
+                    return i++;
+                }},
+            {'targets': 1, 'data': 'question'},
+            {'targets': 2, 'data': 'user_name'},
+            {'targets': 3, 'data': 'createdAt'},
+            {'targets': 4, 'data': 'status'},
             {'targets': [2,3,4], 'width': '15%'},
             {'targets': 5, 'width': '10%'}
         ],
