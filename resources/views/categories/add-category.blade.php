c4d1bc65ce3307937672834d54dadcffcd765952
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-23 20:10:14 +0500

    Creates add category modal view

---
+++ resources/views/categories/add-category.blade.php
@@ -0,0 +1,12 @@
+<div class="modal" id="modal-category">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+            </div>
+            <div class="modal-body">
+
+            </div>
+        </div>
+    </div>
+</div>
