cf5504f6dfbee50cd73897790c807526682501f5
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-23 18:19:57 +0500

    Includes category model by constructor dependency injection

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Category;
 
 class CategoriesController extends Controller
 {
+    public function __construct(
+        private Category $category
+    ) {
+    }
+
     // Default function index
     public function index(): view
     {
