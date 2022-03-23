be31c8bcabdfe4b8916e0f3ead06322340cc23ac
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-23 20:28:37 +0500

    Renders status in category modal

--- resources/views/categories/add-category.blade.php
+++ resources/views/categories/add-category.blade.php
@@ -5,7 +5,18 @@
                 <label>Add Category</label>
             </div>
             <div class="modal-body">
-
+                <div class="row">
+                    <div class="col-12">
+                        <div class="mb-3">
+                            {{Form::label('category', 'Category')}}
+                            {{Form::text('category', null, ['class' => 'form-control', 'id' => 'category'])}}
+                        </div>
+                        <div class="mb-3">
+                            {{Form::label('status', 'Status')}}
+                            {{Form::select('status', $statusArray, '', ['class' => 'form-select'])}}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
