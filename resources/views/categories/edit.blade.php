3a33b7a856c2e58ab0f210a345f4de99688235a1
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-24 15:29:46 +0500

    Includes edit category css

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.main')
+@section('contents')
+
+@push('styles')
+<link rel="stylesheet" href="{{asset('css/category-edit.css')}}" />
+@endpush
+
+{{Form::label('lbl-page-header', $pageTitle, ['id' => 'lbl-page-header'])}}
+
+<div class="page-layout">
+    <div id="dv-category">
+
+    </div>
+</div>
+@endsection
