0fb7f6de99f19a2e8f7a513239f83dcb9cc42fa6
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-24 17:47:17 +0500

    Adds function to initialize subcategories datatbale

--- public/js/category-edit.js
+++ public/js/category-edit.js
@@ -1,3 +1,16 @@
-$(function(){
+$(function() {
     
+    populateSubCategories();
 });
+
+function populateSubCategories() {
+
+    $('#tbl-subcategories').DataTable({
+        'searching': false,
+        'bLengthChange': false,
+        'bSort': false,
+        'language': {
+            'emptyTable': 'No data found'
+        }
+    });
+}
