6dfb9a2a202220332c5d8d8885b0ff87b1628e59
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-24 15:50:22 +0500

    Adds category fields

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -9,7 +9,27 @@
 
 <div class="page-layout">
     <div id="dv-category">
+        {{Form::open()}}
+        <div class="row">
+            <div class="col-4">
+                <div class="mb-3">
+                    {{Form::label('category', 'Category')}}
+                    {{Form::text('category', '', ['class' => 'form-control'])}}
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="mb-3">
+                    {{Form::label('status', 'Status')}}
+                    {{Form::select('status', [], '', ['class' => 'form-select'])}}
+                </div>
+            </div>
+            <div class="col-4 dv-checkbox">
 
+                {{Form::checkbox('publish', '', false)}}
+                {{Form::label('publish', 'Publish')}}
+            </div>
+        </div>
+        {{Form::close()}}
     </div>
 </div>
 @endsection
