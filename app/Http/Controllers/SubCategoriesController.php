51288b940412d8eabe00665cdd0bceeb5ec4512d
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-24 18:19:44 +0500

    Includes subcategory model via constructor dependency injection

--- app/Http/Controllers/SubCategoriesController.php
+++ app/Http/Controllers/SubCategoriesController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuninate\Http\JsonResponse;
+use App\Models\SubCategory;
 
 class SubCategoriesController extends Controller
 {
-    //
+    public function __construct(
+        private SubCategory $subCategory
+    ) {
+    }
+    
+    public function listing($id): JsonResponse
+    {
+    }
 }
