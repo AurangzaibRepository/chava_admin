b2f0f08ac11ea69f6620083136a6da35c862f415
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-25 19:01:39 +0500

    Includes css file

--- resources/views/categories/sub-category.blade.php
+++ resources/views/categories/sub-category.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.main')
 @section('contents')
 
+@push('styles')
+<link rel="stylesheet" href="{{asset('css/subcategory-edit.css')}}" />
+@endpush
+
 {{Form::label('lbl-page-header', $pageTitle , ['id' => 'lbl-page-header'])}}
+
+<div class="page-layout">
+    <div id="dv-subcategory">
+        {{Form::open()}}
+        <div class="row">
+            <div class="col-4">
+                <div class="mb-3">
+                    {{Form::label('subcategory', 'Subcategory')}}
+                    {{Form::text('subcategory', null, ['class' => 'form-control'])}}
+                </div>
+            </div>
+        </div>
+        {{Form::close()}}
+    </div>
+</div>
 
 @endsection
