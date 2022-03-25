e00930d288037aa351cb8f0021031587a1637703
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-25 20:58:32 +0500

    Implements function validate subcategory

--- public/js/subcategory-edit.js
+++ public/js/subcategory-edit.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
     
-})
+});
+
+function validateForm() {
+
+    if ($('#sub_category').val().trim() === '') {
+
+        $('#error-subcategory').css('display', 'block');
+        $('#col-buttons div').css('margin-top', '-3px');
+        return false;
+    }
+}
