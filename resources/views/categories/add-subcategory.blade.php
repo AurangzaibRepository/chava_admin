450d8600cff5759f72617000d9678ce7d5bfd160
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-25 14:32:20 +0500

    Creates subcategory modal

---
+++ resources/views/categories/add-subcategory.blade.php
@@ -0,0 +1,13 @@
+<div class="modal" id="modal-subcategory">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+            </div>
+            <div class="modal-body">
+
+            </div>
+        </div>
+
+    </div>
+</div>
