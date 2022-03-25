2ad37fe73407487ccc1a882b4db8ce44ed51df87
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-25 14:50:47 +0500

    Adds buttons in modal

--- resources/views/categories/add-subcategory.blade.php
+++ resources/views/categories/add-subcategory.blade.php
@@ -7,10 +7,14 @@
             <div class="modal-body">
                 <div class="row">
                     <div class="col-12">
-                        <div class="mb-3">
+                        <div>
                             {{Form::label('subcategory', 'Subcategory')}}
                             {{Form::text('subcategory', null, ['class' => 'form-control'])}}
                         </div>
+                    </div>
+                    <div class="col-12 text-end col-buttons">
+                        {{Form::button('Save', ['class' => 'btn btn-primary'])}}
+                        {{Form::button('Cancel', ['class' => 'btn btn-secondary', 'data-bs-dismiss' => 'modal'])}}
                     </div>
                 </div>
             </div>
