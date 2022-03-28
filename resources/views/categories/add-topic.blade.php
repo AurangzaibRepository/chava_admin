48fd4bda103570942f6e639b24647879b19d9fa3
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-28 15:09:16 +0500

    Adds modal body and type

--- resources/views/categories/add-topic.blade.php
+++ resources/views/categories/add-topic.blade.php
@@ -4,6 +4,16 @@
             <div class="modal-header">
                 {{Form::label('header-label', 'Add Topic')}}
             </div>
+            <div class="modal-body">
+                {{Form::open()}}
+                <div class="row">
+                    <div class="col-12">
+                        {{Form::label('type', 'Type')}}
+                        {{Form::select('type', $topicTypes, null, ['class' => 'form-select'])}}
+                    </div>
+                </div>
+                {{Form::close()}}
+            </div>
         </div>
     </div>
 
