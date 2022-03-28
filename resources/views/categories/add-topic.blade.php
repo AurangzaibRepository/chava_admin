17a32735ce936b8f8fb6ab550d72703edd4ec111
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-28 14:49:08 +0500

    Adds modal header

--- resources/views/categories/add-topic.blade.php
+++ resources/views/categories/add-topic.blade.php
@@ -1,7 +1,9 @@
 <div class="modal" id="modal-add-topic">
     <div class="modal-dialog">
         <div class="modal-content">
-
+            <div class="modal-header">
+                {{Form::label('header-label', 'Add Topic')}}
+            </div>
         </div>
     </div>
 
