5c3ba6199ea270ece73617aac3ab55ec6afdcaf5
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-28 12:50:14 +0500

    Includes topic model via dependency injection

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\Topic;
 
 class TopicsController extends Controller
 {
-    //
+    public function __construct(
+        private Topic $topic
+    ) {
+    }
+
+    public function listing(Request $request): JsonResponse
+    {
+    }
 }
