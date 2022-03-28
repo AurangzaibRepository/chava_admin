5e0282bb144743f0c1a6eeaf813c1a9f7ef0415f
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-03-28 13:49:55 +0500

    Implements function to retrieve topics

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 
 class Topic extends Model
 {
     protected $table = 'sub_category_topics';
+
+    public function getListing(int $subCategoryID): JsonResponse
+    {
+        $data = [
+            'data' => []
+        ];
+
+        $topicList = $this
+                        ->where('sub_category_id', $subCategoryID)
+                        ->get();
+
+        foreach ($topicList as $key => $value) {
+            $data['data'][] = [
+                ($key+1),
+                $value->topic,
+                $value->type,
+                $value->id
+            ];
+        }
+
+        return response()->json($data);
+    }
 }
