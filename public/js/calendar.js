6473fefbf1945c88874a319b5f47042a7edd71c4
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-05 17:37:38 +0500

    Implements function to initialize table

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,4 +1,19 @@
 $(function(){
 
-    
+    populateReminders();
 });
+
+function populateReminders() {
+
+    $('#table-reminders').DataTable({
+
+        'searching': false,
+        'bLengthChange': false,
+        'bSort': false,
+        'columnDefs': [{
+            'targets': 0, 
+            'width': '8%',
+            'class': 'text-center'
+        }]
+    });
+}
