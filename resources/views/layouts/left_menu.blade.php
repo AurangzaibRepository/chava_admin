d51696eb24e94a27689b0e4cc77b0e8bfa08b8a7
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-05 16:31:52 +0500

    Specifies calendar link in left menu

--- resources/views/layouts/left_menu.blade.php
+++ resources/views/layouts/left_menu.blade.php
@@ -1,24 +1,37 @@
 <ul class="ul-left-menu">
     <li>
-        <a href="{{route('dashboard')}}" class="dashboard"> <i class="fas fa-chart-pie"></i> <span>Dashboard</span></a>
+        <a href="{{route('dashboard')}}" class="dashboard">
+            <i class="fas fa-chart-pie"></i> <span>Dashboard</span>
+        </a>
     </li>
     <li>
-        <a href="{{route('users')}}" class="users"><i class="fa fa-users"></i> <span>Manage Users</span></a>
+        <a href="{{route('users')}}" class="users">
+            <i class="fa fa-users"></i> <span>Manage Users</span>
+        </a>
     </li>
     <li>
-        <a href="{{route('whatsappSessions')}}" class="whatsapp-sessions"><i class="far fa-file-alt"></i> <span>Whatsapp
-                Sessions</span></a>
+        <a href="{{route('whatsappSessions')}}" class="whatsapp-sessions">
+            <i class="far fa-file-alt"></i> <span>Whatsapp Sessions</span>
+        </a>
     </li>
     <li>
-        <a href="{{route('community')}}" class="community"><i class="fas fa-cog"></i><span>Manage Community</span></a>
+        <a href="{{route('community')}}" class="community">
+            <i class="fas fa-cog"></i><span>Manage Community</span>
+        </a>
     </li>
     <li>
-        <a href="{{route('topics')}}" class="topics"><i class="fas fa-fan"></i><span>Manage Topics</span></a>
+        <a href="{{route('topics')}}" class="topics">
+            <i class="fas fa-fan"></i><span>Manage Topics</span>
+        </a>
     </li>
     <li>
-        <a class="ads"><i class="far fa-file-alt"></i><span> Manage Calendar</span></a>
+        <a href="{{route('calendar')}}" class="calendar">
+            <i class="far fa-file-alt"></i><span> Manage Calendar</span>
+        </a>
     </li>
     <li>
-        <a href="{{route('logout')}}">&nbsp;<i class="fas fa-sign-out-alt"></i><span> Logout</span></a>
+        <a href="{{route('logout')}}">&nbsp;
+            <i class="fas fa-sign-out-alt"></i><span> Logout</span>
+        </a>
     </li>
 </ul>
