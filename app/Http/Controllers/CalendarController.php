dbbbe7bd68a858e97bfe23cf7117f24aaaf2c6f7
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-06 11:30:55 +0500

    Includes reminder model via dependency injection

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
+use App\Models\UserReminder;
 
 class CalendarController extends Controller
 {
+    public function __construct(
+        private UserReminder $reminder
+    ) {
+    }
+
     public function index(): view
     {
         return view('calendar.listing')->with([
             'pageTitle' => 'Calendar'
         ]);
+    }
+
+    public function listing(Request $request) : JsonResponse
+    {
+        $this->reminder->getListing();
     }
 }
