0e86623449610f006bb28f51ec6934ae1cb6a2e7
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-07 17:32:09 +0500

    Show loader on delete topic

--- public/js/topic-edit.js
+++ public/js/topic-edit.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     $('#modal-delete-topic').on('shown.bs.modal', (e) => {
         
+       $('#modal-delete-topic .fa-spinner').css('display', 'none');
        $('#modal-delete-topic [name="topic_id"]').val($(e.relatedTarget).data('topicid') );
        $('#modal-delete-topic #spn-topic').text($(e.relatedTarget).data('topic'));
     });
@@ -9,6 +10,8 @@ $(document).ready(function(){
 
 
 function deleteTopic() {
+
+    $('#modal-delete-topic .fa-spinner').css('display', 'block');
 
     $.ajax({
         type: 'DELETE',
