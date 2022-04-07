d1adbabf0a5b1b006b8031e8f653199d8199fee0
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-07 13:56:23 +0500

    Adds text and buttons in delete modal

--- resources/views/categories/delete-topic.blade.php
+++ resources/views/categories/delete-topic.blade.php
@@ -4,6 +4,17 @@
             <div class="modal-header">
                 {{Form::label('header-label', 'Delete Topic')}}
             </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label>Are you sure you want to delete this topic?</label>
+                    </div>
+                    <div class="col-12 text-end">
+                        {{Form::button('Yes', ['class' => 'btn btn-primary'])}}
+                        {{Form::button('No', ['class' => 'btn btn-secondary'])}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
