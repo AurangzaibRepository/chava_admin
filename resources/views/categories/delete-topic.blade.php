c91e472e3ca13cf2eb332a536eaf11182562c1f8
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-07 17:26:37 +0500

    Adds loader for delete modal

--- resources/views/categories/delete-topic.blade.php
+++ resources/views/categories/delete-topic.blade.php
@@ -5,6 +5,7 @@
                 {{Form::label('header-label', 'Delete Topic')}}
             </div>
             <div class="modal-body">
+                <i class="fas fa-spin fa-spinner"></i>
                 <div class="row">
                     <div class="col-12 mb-3">
                         {{Form::hidden('topic_id')}}
