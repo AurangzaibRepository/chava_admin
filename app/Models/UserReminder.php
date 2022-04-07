d08bd8de37ca03ea3a6a382dad69f7c4c1804e17
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-07 12:59:47 +0500

    Implements function to retrieve reminders

--- app/Models/UserReminder.php
+++ app/Models/UserReminder.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class UserReminder extends Model
 {
     protected $table = 'user_reminders';
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+
+    public function getListing(Request $request): JsonResponse
+    {
+        $response = [
+            'draw' => 1,
+            'recordsTotal' => 20,
+            'recordsFiltered' => 20,
+            'data' => []
+        ];
+
+        $data = $this
+                    ->join('users', 'users.id', 'user_reminders.user_id')
+                    ->select('user_reminders.*', 'users.user_name')
+                    ->orderBy('id', 'desc')
+                    ->get();
+
+        foreach ($data as $key => $value) {
+            $response['data'][] = [
+                ($key+1),
+                $value->reminder,
+                $value->place,
+                "{$value->date} {$value->time}",
+                $value->user_name,
+                $value->id
+            ];
+        }
+
+        return response()->json($response);
+    }
 }
