141de68e2b796a7d8a28e9d874bf0e7341d87880
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-07 13:48:11 +0500

    Creates delete topic modal file

---
+++ resources/views/categories/delete-topic.blade.php
@@ -0,0 +1,9 @@
+<div class="modal" id="modal-delete-topic">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                {{Form::label('header-label', 'Delete Topic')}}
+            </div>
+        </div>
+    </div>
+</div>
