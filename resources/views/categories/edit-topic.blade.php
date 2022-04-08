c85a0f0cebac652eda3973e3fd008831187627a5
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-08 17:28:59 +0500

    Adds buttons in edit modal

--- resources/views/categories/edit-topic.blade.php
+++ resources/views/categories/edit-topic.blade.php
@@ -14,12 +14,16 @@
                         {{Form::label('title', 'Title')}}
                         {{Form::text('title', null, ['class' => 'form-control'])}}
                     </div>
-                    <div class="col-12 mb-3">
+                    <div class="col-12">
                         {{Form::label('video', 'Select File')}}
                         <div class="embed-responsive embed-responsive-16by9">
                             <iframe class="embed-responsive-item" id="iframe-video" allowfullscreen></iframe>
                         </div>
                         {{Form::file('video')}}
+                    </div>
+                    <div class="col-12 text-end">
+                        {{Form::button('Save', ['class' => 'btn btn-primary'])}}
+                        {{Form::button('Cancel', ['class' => 'btn btn-secondary'])}}
                     </div>
                 </div>
             </div>
