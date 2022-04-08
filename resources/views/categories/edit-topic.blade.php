398ae5ada64c51643c6f9d6f7c0de7a4d5c6df00
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-08 16:54:10 +0500

    Adds edit modal header

--- resources/views/categories/edit-topic.blade.php
+++ resources/views/categories/edit-topic.blade.php
@@ -1,7 +1,9 @@
 <div class="modal" id="modal-edit-topic">
     <div class="modal-dialog">
         <div class="modal-content">
-
+            <div class="modal-header">
+                {{Form::label('header-label', 'Edit Topic')}}
+            </div>
         </div>
     </div>
 </div>
