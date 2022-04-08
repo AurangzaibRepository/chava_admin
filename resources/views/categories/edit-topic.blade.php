7daf75a57276d283af75632d43db4b46e086413f
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-04-08 17:08:24 +0500

    Adds title and type fields

--- resources/views/categories/edit-topic.blade.php
+++ resources/views/categories/edit-topic.blade.php
@@ -4,6 +4,18 @@
             <div class="modal-header">
                 {{Form::label('header-label', 'Edit Topic')}}
             </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        {{Form::label('type', 'Type')}}
+                        {{Form::select('type', [], null, ['class' => 'form-select'])}}
+                    </div>
+                    <div class="col-12 mb-3">
+                        {{Form::label('title', 'Title')}}
+                        {{Form::text('title', null, ['class' => 'form-control'])}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
