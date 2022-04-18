aed990d59f42adbb5579430d237783db2fb0e475
Aurangzaib najam <aurangzaibzaee@gmail.com>
2022-04-18 17:35:04 +0500

    Adds view and deleet icon

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -11,10 +11,22 @@ function populateReminders() {
         'bLengthChange': false,
         'bSort': false,
         'columnDefs': [{
-            'targets': 0, 
+            'targets': 0,
             'width': '8%',
             'class': 'text-center'
-        }],
+        },
+        {
+            'targets': 5,
+            'class': 'text-center',
+            'width': '10%',
+            render: function(data, row) {
+                return `
+                        <a> <i class="fa fa-eye"></i></a>
+                        <a> <i class="fa fa-times"></i> </a>
+                        `;
+            }
+        }
+        ],
         'serverSide': true,
         'ajax': '/calendar/listing'
     });
