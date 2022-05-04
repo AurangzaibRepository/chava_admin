1277849ad2c5cdd0614a9f5ea294655a85d3a82c
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-04 14:04:59 +0500

    Specifies flex center classes

--- resources/views/calendar/add.blade.php
+++ resources/views/calendar/add.blade.php
@@ -7,21 +7,21 @@
     <div class="page-layout">
         {{Form::open()}}
         <div class="row mb-3">
-            <div class="col-2">{{Form::label('reminder', 'Reminder')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('reminder', 'Reminder')}}</div>
             <div class="col-4">{{Form::select('reminder', [], null, ['class' => 'form-control'])}}</div>
-            <div class="col-2">{{Form::label('place', 'Place')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('place', 'Place')}}</div>
             <div class="col-4">{{Form::text('place', '', ['class' => 'form-control'])}}</div>
         </div>
         <div class="row mb-3">
-            <div class="col-2">{{Form::label('grades', 'Grades')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('grades', 'Grades')}}</div>
             <div class="col-4">{{Form::text('grades', null, ['class' => 'form-control'])}}</div>
-            <div class="col-2">{{Form::label('priorities', 'Priorities')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('priorities', 'Priorities')}}</div>
             <div class="col-4">{{Form::select('priorities', [], null, ['class' => 'form-control'])}}</div>
         </div>
         <div class="row mb-3">
-            <div class="col-2">{{Form::label('date', 'Date')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
             <div class="col-4">{{Form::text('date', null, ['class' => 'form-control'])}}</div>
-            <div class="col-2">{{Form::label('time', 'Time')}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
             <div class="col-4">{{Form::text('time', null, ['class' => 'form-control'])}}</div>
         </div>
         {{Form::close()}}
