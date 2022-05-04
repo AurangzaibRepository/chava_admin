cd4e5dba30fe2bbe9d55ea85c5543705c3649d9c
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-04 16:39:49 +0500

    Makes datepicker readonly

--- resources/views/calendar/add.blade.php
+++ resources/views/calendar/add.blade.php
@@ -35,7 +35,7 @@
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
-            <div class="col-4">{{Form::text('date', null, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::text('date', null, ['class' => 'form-control', 'readonly' => 'readonly'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
             <div class="col-4">{{Form::text('time', null, ['class' => 'form-control'])}}</div>
         </div>
