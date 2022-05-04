ddda6def3d31985aea0a5bd2227f1ea011d83223
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-04 17:11:01 +0500

    Initializes time picker

--- public/js/calendar-add.js
+++ public/js/calendar-add.js
@@ -1,6 +1,7 @@
 $(function() {
     
     initializeDatepicker();
+    initializeTimepicker();
 });
 
 function initializeDatepicker() {
@@ -18,4 +19,20 @@ function initializeDatepicker() {
     $('#date').on('hide.daterangepicker', function(ev, picker) {
         $('#date').val(picker.startDate.format('DD/MM/YYYY'));
     });
+}
+
+function initializeTimepicker() {
+    $('#time').daterangepicker({
+        singleDatePicker: true,
+        timePicker: true,
+        timePicker24Hour: true,
+        timePickerSeconds: true,
+        locale: {
+            format: 'HH:mm:ss'
+        }
+    }).on('show.daterangepicker', function(ev, picker) {
+        picker.container.find('.calendar-table').hide();
+    });
+
+    $('#time').val('');
 }
