a2547541514ab88deb5303a63a7a6f48e841e47e
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-04 13:38:37 +0500

    Adds form fields

--- resources/views/calendar/add.blade.php
+++ resources/views/calendar/add.blade.php
@@ -3,4 +3,23 @@
 
 {{Form::label('lbl-page-header', $pageTitle, ['id' => 'lbl-page-header'])}}
 
+<div class="dv-base">
+    <div class="page-layout">
+        {{Form::open()}}
+        <div class="row mb-3">
+            <div class="col-2">{{Form::label('reminder', 'Reminder')}}</div>
+            <div class="col-4">{{Form::select('reminder', [], null, ['class' => 'form-control'])}}</div>
+            <div class="col-2">{{Form::label('place', 'Place')}}</div>
+            <div class="col-4">{{Form::text('place', '', ['class' => 'form-control'])}}</div>
+        </div>
+        <div class="row">
+            <div class="col-2">{{Form::label('grades', 'Grades')}}</div>
+            <div class="col-4">{{Form::text('grades', null, ['class' => 'form-control'])}}</div>
+            <div class="col-2">{{Form::label('priorities', 'Priorities')}}</div>
+            <div class="col-4">{{Form::select('priorities', [], null, ['class' => 'form-control'])}}</div>
+        </div>
+        {{Form::close()}}
+    </div>
+</div>
+
 @endsection
