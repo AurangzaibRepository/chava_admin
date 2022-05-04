ea19e343229207de041234cf9fb82b58b2e0a319
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-04 14:17:35 +0500

    Adds repeat and user fields

--- resources/views/calendar/add.blade.php
+++ resources/views/calendar/add.blade.php
@@ -8,7 +8,7 @@
         {{Form::open()}}
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('reminder', 'Reminder')}}</div>
-            <div class="col-4">{{Form::select('reminder', [], null, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::select('reminder', [], null, ['class' => 'form-select'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('place', 'Place')}}</div>
             <div class="col-4">{{Form::text('place', '', ['class' => 'form-control'])}}</div>
         </div>
@@ -16,13 +16,19 @@
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('grades', 'Grades')}}</div>
             <div class="col-4">{{Form::text('grades', null, ['class' => 'form-control'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('priorities', 'Priorities')}}</div>
-            <div class="col-4">{{Form::select('priorities', [], null, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::select('priorities', [], null, ['class' => 'form-select'])}}</div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
             <div class="col-4">{{Form::text('date', null, ['class' => 'form-control'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
             <div class="col-4">{{Form::text('time', null, ['class' => 'form-control'])}}</div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('repeat', 'Repeat')}}</div>
+            <div class="col-4">{{Form::select('repeat', [], null, ['class' => 'form-select'])}}</div>
+            <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('userid', 'User')}}</div>
+            <div class="col-4">{{Form::select('userid', [], null, ['class' => 'form-select'])}}</div>
         </div>
         {{Form::close()}}
     </div>
