89c6bbf78b6dd58369cbfb4ae70ae6c2f8d7bf24
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-05 19:14:17 +0500

    Displays reminder data

--- resources/views/calendar/edit.blade.php
+++ resources/views/calendar/edit.blade.php
@@ -18,27 +18,32 @@
         {{Form::open(['route' => 'saveCalendar'])}}
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('reminder', 'Reminder')}}</div>
-            <div class="col-4">{{Form::select('reminder', $reminder, null, ['class' => 'form-select'])}}</div>
+            <div class="col-4">{{Form::select('reminder', $reminder, $data->reminder, ['class' => 'form-select'])}}
+            </div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('place', 'Place')}}</div>
-            <div class="col-4">{{Form::text('place', '', ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::text('place', $data->place, ['class' => 'form-control'])}}</div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('grades', 'Grades')}}</div>
-            <div class="col-4">{{Form::text('grades', null, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::text('grades', $data->grades, ['class' => 'form-control'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('priorities', 'Priorities')}}</div>
-            <div class="col-4">{{Form::select('priorities', $priorities, null, ['class' => 'form-select'])}}</div>
+            <div class="col-4">{{Form::select('priorities', $priorities, $data->priorities, ['class' =>
+                'form-select'])}}</div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
-            <div class="col-4">{{Form::text('date', null, ['class' => 'form-control', 'readonly' => 'readonly'])}}</div>
+            <div class="col-4">{{Form::text('date', null, ['class' => 'form-control', 'readonly' =>
+                'readonly'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
-            <div class="col-4">{{Form::text('time', null, ['class' => 'form-control', 'readonly' => 'readonly'])}}</div>
+            <div class="col-4">{{Form::text('time', null, ['class' => 'form-control', 'readonly' =>
+                'readonly'])}}</div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('repeat', 'Repeat')}}</div>
-            <div class="col-4">{{Form::select('repeat', $repeat, null, ['class' => 'form-select'])}}</div>
+            <div class="col-4">{{Form::select('repeat', $repeat, $data->repeat, ['class' => 'form-select'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('user_id', 'User')}}</div>
-            <div class="col-4">{{Form::select('user_id', ['' => '-- Select --'], '', ['class' => 'form-select'])}}</div>
+            <div class="col-4">{{Form::select('user_id', ['' => '-- Select --'], '', ['class' =>
+                'form-select'])}}</div>
         </div>
         <div class="row">
             <div class="col-12 d-flex flex-wrap justify-content-end">
