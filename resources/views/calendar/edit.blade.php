c2f12aaebf8c2f907012f3998a12ab0c89f5c6ff
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-06 16:30:19 +0500

    Adds hidden field for date

--- resources/views/calendar/edit.blade.php
+++ resources/views/calendar/edit.blade.php
@@ -32,8 +32,10 @@
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
-            <div class="col-4">{{Form::text('date', null, ['class' => 'form-control', 'readonly' =>
-                'readonly'])}}</div>
+            <div class="col-4">
+                {{Form::text('date', '', ['class' => 'form-control', 'readonly' =>'readonly'])}}
+                {{Form::hidden('hdn_date', $data->date)}}
+            </div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
             <div class="col-4">{{Form::text('time', null, ['class' => 'form-control', 'readonly' =>
                 'readonly'])}}</div>
