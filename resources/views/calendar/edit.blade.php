82dc3c4e71995d3b3a7d33b87ed7fb50bb736819
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-09 17:24:26 +0500

    Specifies old input values

--- resources/views/calendar/edit.blade.php
+++ resources/views/calendar/edit.blade.php
@@ -5,35 +5,37 @@
 <link rel="stylesheet" href="{{asset('css/daterangepicker.css')}}" />
 <link rel="stylesheet" href="{{asset('css/select2.min.css')}}" />
 <link rel="stylesheet" href="{{asset('css/calendar-add.css')}}" />
+<link rel="stylesheet" href="{{asset('css/calendar-edit.css')}}" />
 @endpush
 
 {{Form::label('lbl-page-header', $pageTitle, ['id' => 'lbl-page-header'])}}
 
-@if(session()->has('success'))
-<div class="alert alert-success">{{session()->get('success')}}</div>
-@endif
+@include('calendar.messages')
 
 <div class="dv-base">
     <div class="page-layout">
         {{Form::open(['route' => 'saveCalendar'])}}
         {{Form::hidden('id', $data->id)}}
-        {{Form::hidden('hdn_date', $data->date)}}
-        {{Form::hidden('hdn_time', $data->time)}}
-        {{Form::hidden('hdn_userid', $data->user_id)}}
+        {{Form::hidden('hdn_date', old('date', $data->date) )}}
+        {{Form::hidden('hdn_time', old('time', $data->time))}}
+        {{Form::hidden('hdn_userid', old('user_id', $data->user_id) )}}
 
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('reminder', 'Reminder')}}</div>
-            <div class="col-4">{{Form::select('reminder', $reminder, $data->reminder, ['class' => 'form-select'])}}
+            <div class="col-4">{{Form::select('reminder', $reminder, old('reminder', $data->reminder), ['class' =>
+                'form-select'])}}
             </div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('place', 'Place')}}</div>
-            <div class="col-4">{{Form::text('place', $data->place, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::text('place', old('place', $data->place), ['class' => 'form-control'])}}</div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('grades', 'Grades')}}</div>
-            <div class="col-4">{{Form::text('grades', $data->grades, ['class' => 'form-control'])}}</div>
+            <div class="col-4">{{Form::text('grades', old('grades', $data->grades), ['class' => 'form-control'])}}</div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('priorities', 'Priorities')}}</div>
-            <div class="col-4">{{Form::select('priorities', $priorities, $data->priorities, ['class' =>
-                'form-select'])}}</div>
+            <div class="col-4">{{Form::select('priorities', $priorities, old('priorities', $data->priorities), ['class'
+                =>
+                'form-select'])}}
+            </div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('date', 'Date')}}</div>
@@ -42,11 +44,14 @@
             </div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('time', 'Time')}}</div>
             <div class="col-4">{{Form::text('time', null, ['class' => 'form-control', 'readonly' =>
-                'readonly'])}}</div>
+                'readonly'])}}
+            </div>
         </div>
         <div class="row mb-3">
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('repeat', 'Repeat')}}</div>
-            <div class="col-4">{{Form::select('repeat', $repeat, $data->repeat, ['class' => 'form-select'])}}</div>
+            <div class="col-4">{{Form::select('repeat', $repeat, old('repeat', $data->repeat), ['class' =>
+                'form-select'])}}
+            </div>
             <div class="col-2 d-flex flex-wrap align-content-center">{{Form::label('user_id', 'User')}}</div>
             <div class="col-4">{{Form::select('user_id', ['' => '-- Select --'], '', ['class' =>'form-select'])}}</div>
         </div>
