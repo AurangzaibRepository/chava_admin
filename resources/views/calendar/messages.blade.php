9046d5be93bda9f99c14b5c956aa296c0a427295
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-10 12:47:05 +0500

    Removes errors display

--- resources/views/calendar/messages.blade.php
+++ resources/views/calendar/messages.blade.php
@@ -1,11 +1,3 @@
 @if(session()->has('success'))
 <div class="alert alert-success">{{session()->get('success')}}</div>
 @endif
-
-@if ($errors->any())
-<div class="alert alert-danger">
-    @foreach ($errors->all() as $error)
-    <span>{{ucfirst($error)}}</span> <br />
-    @endforeach
-</div>
-@endif
