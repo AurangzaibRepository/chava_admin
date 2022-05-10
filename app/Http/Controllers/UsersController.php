26bf2d5f53629d0e6e161f5163183b84801af901
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-10 16:26:45 +0500

    Implements route delete function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -47,5 +47,11 @@ class UsersController extends Controller
     public function all(): JsonResponse
     {
         return $this->user->get();
+    }
+
+    public function delete(int $id): RedirectResponse
+    {
+        $this->user->deleteRecord($id);
+        return redirect()->to('/users')->with('success', 'User deleted successfully');
     }
 }
