a24df123c77564d8e14a10e7087e951cd3efc5f4
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-05-11 14:41:52 +0500

    Removes function to preview image

--- public/js/subcategory-edit.js
+++ public/js/subcategory-edit.js
@@ -202,9 +202,3 @@ function showProgress(event) {
         }
     }
 }
-
-function previewThumbnail(obj) {
-    
-    $('#img-thumbnail').css('display', 'block');
-    $('#img-thumbnail').attr('src', URL.createObjectURL(obj.files[0]) );
-}
