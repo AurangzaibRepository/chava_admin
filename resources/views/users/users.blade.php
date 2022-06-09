1929116fe9480ca0248e92a7ae36a014aaa730af
Aurnagzaib Najam <aurangzaibzaee@gmail.com>
2022-06-09 14:28:50 +0500

    Includes user momdal

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -35,43 +35,7 @@
 </div>
 
 {{-- User detail modal --}}
-<div class="modal" id="modal-user">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-5"><img class="img-user" src="{{asset('/images/leaf-icon.png')}}"></img></div>
-                    <div class="col-7 dv-user-info">
-                        <span id="name"></span><br />
-                        <span>Country - <span id="country">Nicaragua</span></span>
-                    </div>
-                </div>
-                <br />
-                <table id="tbl-user-info">
-                    <tbody>
-                        <tr>
-                            <td>Date of Joining</td>
-                            <td><span id="registered-date"></span></td>
-                        </tr>
-                        <tr>
-                            <td>Last Active</td>
-                            <td><span id="last-active"></span></td>
-                        </tr>
-                        <tr>
-                            <td>Phone number</td>
-                            <td><span id="phone-no"></span></td>
-                        </tr>
-                    </tbody>
-
-                </table>
-            </div> <!-- modal body -->
-        </div> <!-- modal-content -->
-    </div>
-</div>
+@include('users.modal-user')
 
 @endsection
 
@@ -79,6 +43,5 @@
 @push('scripts')
 <script src="{{URL::asset('js/moment.js')}}"></script>
 <script src="{{URL::asset('js/daterangepicker.min.js')}}"></script>
-
 <script src="{{asset('js/users.js')}}"></script>
 @endpush
