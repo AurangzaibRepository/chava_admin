c6ba866b12062948c1bb53d080ad54d9ab440523
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-06-09 16:20:18 +0500

    Adds placeholder

--- resources/views/whatsapp_sessions/filters.blade.php
+++ resources/views/whatsapp_sessions/filters.blade.php
@@ -2,10 +2,10 @@
     {{Form::open()}}
     <div class="row">
         <div class="col-md-4">
-
+            {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Name'])}}
         </div>
         <div class="col-md-4">
-
+            {{Form::text('phone', '', ['class' => 'form-control', 'placeholder' => 'Phone Number'])}}
         </div>
     </div>
     {{Form::close()}}
