110e043c7588612f4646d69c5bb4a7584266bcf5
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-06-22 14:56:59 +0500

    Includes column div

--- resources/views/calendar/filters.blade.php
+++ resources/views/calendar/filters.blade.php
@@ -1,3 +1,16 @@
 <div class="dv-filter">
+    <div class="row">
+        <div class="col-md-4 mb-3">
 
+        </div>
+        <div class="col-md-4 mb-3">
+
+        </div>
+        <div class="col-md-4 mb-3">
+
+        </div>
+        <div class="col-md-4 mb-3">
+
+        </div>
+    </div>
 </div>
