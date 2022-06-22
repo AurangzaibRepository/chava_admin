47194a6bc09a67c63370164d5461067f6e8778e0
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-06-22 15:42:30 +0500

    Adds button

--- resources/views/calendar/filters.blade.php
+++ resources/views/calendar/filters.blade.php
@@ -1,5 +1,5 @@
 <div class="dv-filter">
-    {{Form::open()}}
+    {{Form::open(['id' => 'form-filter'])}}
     <div class="row">
         <div class="col-md-4 mb-3">
             {{Form::text('reminder', null, ['class' => 'form-control', 'placeholder' => 'Reminder'])}}
@@ -13,6 +13,10 @@
         <div class="col-md-4 mb-3">
             {{Form::text('datetime', null, ['class' => 'form-control', 'placeholder' => 'Datetime'])}}
         </div>
+        <div class="col-md-8 mb-3 d-flex align-items-end justify-content-end">
+            {{Form::button('Search', ['class' => 'btn btn-primary'])}}
+            {{Form::button('Reset', ['class' => 'btn btn-secondary'])}}
+        </div>
     </div>
     {{Form::close()}}
-</div>
+</div
