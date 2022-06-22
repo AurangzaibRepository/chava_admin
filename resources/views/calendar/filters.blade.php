481ba64d0e207698aaf76d8620e3f9acef9f79b8
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-06-22 15:07:22 +0500

    Adds reminder and place filter box

--- resources/views/calendar/filters.blade.php
+++ resources/views/calendar/filters.blade.php
@@ -1,10 +1,10 @@
 <div class="dv-filter">
     <div class="row">
         <div class="col-md-4 mb-3">
-
+            {{Form::text('reminder', null, ['class' => 'form-control', 'placeholder' => 'Reminder'])}}
         </div>
         <div class="col-md-4 mb-3">
-
+            {{Form::text('place', null, ['class' => 'form-control', 'placeholder' => 'Place'])}}
         </div>
         <div class="col-md-4 mb-3">
 
