5a3ea8e64170deb4a2603a6ed1f5c214a8ad2544
Aurangzaib Najam <aurangzaibzaee@gmail.com>
2022-06-26 15:14:05 +0500

    Removes authorize function

--- app/Http/Requests/CalendarPostRequest.php
+++ app/Http/Requests/CalendarPostRequest.php
@@ -6,16 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CalendarPostRequest extends FormRequest
 {
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
